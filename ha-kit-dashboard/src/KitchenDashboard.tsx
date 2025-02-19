2d2e6a70a3a966c6acaae1e48e60a51cbce90bcf
Jonny Davey <Jonny190@gmail.com>
2025-02-19 13:36:08 +0000

    Inital Changes

---
+++ ha-kit-dashboard/src/KitchenDashboard.tsx
@@ -0,0 +1,52 @@
+import { Group, Row, ButtonCard, Column, ClimateCard, FamilyCard, PersonCard, AlarmCard  } from '@hakit/components';
+import { useHass } from "@hakit/core";
+
+function Dashboard() {
+  const { getAllEntities } = useHass();
+  return <Column fullWidth fullHeight>
+  <Row fullWidth>
+    <Group title="Info">
+      <ClimateCard
+        entity="climate.kitchen_heating"
+        hvacModes={[
+         'heat'
+        ]}
+        hideFanModes
+        hideHvacModes
+        hideSwingModes
+        hidePresetModes
+        layoutType="slim-vertical"
+        showTemperatureControls
+        hideIcon
+        hideState
+      />
+      <FamilyCard title="Who's Home">
+        <PersonCard entity="person.jonny" />
+        <PersonCard entity="person.alex" />
+      </FamilyCard>
+      <AlarmCard
+        defaultCode={2982}
+        entity="alarm_control_panel.alarmo"
+      />
+    </Group>
+  </Row>
+  <Row fullWidth>
+    <Group title="Kitchen Lights">
+      <ButtonCard
+        entity="light.kitchen_uplights"
+        service="toggle"
+      />
+      <ButtonCard
+        entity="light.kitchen_downlights"
+        service="toggle"
+      />
+      <ButtonCard
+        entity="light.kitchen_down_lights"
+        service="toggle"
+      />
+    </Group>
+  </Row>
+  </Column>
+}
+
+export default Dashboard
